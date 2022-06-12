c38c39d01a9fbb31d3d96df7336a880cd6a15d7d
Veronika Maňhalová <68202646+veronikamanhalova@users.noreply.github.com>
2022-06-13 00:41:17 +0200

    Update source.java

--- source.java
+++ source.java
@@ -1,11 +1,3 @@
-/*
- * File: BlankClass.java
- * ---------------------
- * This class is a blank one that you can change at will. Remember, if you
- * change the class name, you'll need to change the filename so that it matches.
- * Then you can extend GraphicsProgram, ConsoleProgram, or DialogProgram as you
- * like.
- */
 import acm.graphics.GImage;
 import acm.graphics.GLabel;
 import acm.graphics.GOval;
